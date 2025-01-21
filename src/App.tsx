227adeb44fd3b9d5715b76fc75b40d5b427294aa
Heet Vadiya <heetvadiya@gmail.com>
2025-01-21 18:54:19 +0530

    [Add] OKRs adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,165 @@
 import {useState} from "react";
+import {Frown} from "lucide-react";
+
+type KeyResultType = {
+  title: string,
+  initialValue: number,
+  currentValue: number,
+  targetValue: number
+  metrics: string
+}
+
+type OKRType = {
+  objective: string
+  keyResults: KeyResultType[];
+}
+
 
 function App() {
-    const initialObjectives: string[] = [
-        // "Build a frontend devloper team",
-        // "Build a Backend devloper team",
-        // "Build a testing team",
-    ];
+  const newKeyResult: KeyResultType = {
+    title: "",
+    initialValue: 0,
+    currentValue: 0,
+    targetValue: 0,
+    metrics: "",
+  }
 
-    const [objectives, setObjectives] = useState<String[]>([]);
-    const [newObjective, setNewObjective] = useState<String>("");
+  const [newObjective, setNewObjective] = useState<string>("");
+  const [keyResults, setKeyResults] = useState<KeyResultType[]>([newKeyResult]);
+  const [okrs, setOkrs] = useState<OKRType[]>([]);
 
-    function addObjective() {
-        setObjectives([...objectives, newObjective]);
+  function addObjective() {
+    let newOKR: OKRType = {
+      objective: newObjective,
+      keyResults: keyResults
     }
+    setOkrs([...okrs, newOKR]);
+  }
 
-    return (
-        <>
-            <div className="mx-8 my-4">
-                <div className="border-black border-2 px-4 py-8 space-y-4">
-                    <p className="font-bold">Create Objective Form</p>
-                    <div className="space-y-4">
-                        <div className="space-x-4 max-w-2xl px-4">
-                            <label htmlFor="objective-input" className="ml-4">New Objective</label>
-                            <input
-                                className="w-full px-1 py-2 border border-amber-400 focus:border-none rounded-md focus:outline-0 focus:ring-blue-500 focus:ring-1 "
-                                type="text"
-                                placeholder="Add Objective"
-                                onChange={(e) => setNewObjective(e.target.value)}/>
-                        </div>
-                        <div className="space-x-4 max-w-2xl px-4">
-                            <label htmlFor="objective-input" className="ml-4">New KeyResults</label>
-                            <input
-                                className="w-full px-1 py-2 border border-amber-400 focus:border-none rounded-md focus:outline-0 focus:ring-blue-500 focus:ring-1 "
-                                type="text"
-                                placeholder="Add KeyResults"
-                                onChange={(e) => setNewObjective(e.target.value)}/>
-                        </div>
-                        <div className="flex ml-4">
+  function addNewKeyResult() {
+    setKeyResults([...keyResults, newKeyResult]);
+  }
 
-                            <div className="space-x-4 max-w-2xl px-4">
-                                <input
-                                    className="w-full px-1 py-2 border border-amber-400 focus:border-none rounded-md focus:outline-0 focus:ring-blue-500 focus:ring-1 "
-                                    type="text"
-                                    placeholder="Initial Value"
-                                    onChange={(e) => setNewObjective(e.target.value)}/>
-                            </div>
-                            <div className="space-x-4 max-w-2xl px-4">
-                                <input
-                                    className="w-full px-1 py-2 border border-amber-400 focus:border-none rounded-md focus:outline-0 focus:ring-blue-500 focus:ring-1 "
-                                    type="text"
-                                    placeholder="Current Value"
-                                    onChange={(e) => setNewObjective(e.target.value)}/>
-                            </div>
-                            <div className="space-x-4 max-w-2xl px-4">
-                                <input
-                                    className="w-full px-1 py-2 border border-amber-400 focus:border-none rounded-md focus:outline-0 focus:ring-blue-500 focus:ring-1 "
-                                    type="text"
-                                    placeholder="Target Value"
-                                    onChange={(e) => setNewObjective(e.target.value)}/>
-                            </div>
-                        </div>
-                        <div className="space-x-4 max-w-2xl px-4">
-                            <input
-                                className="px-1 py-2 border border-amber-400 focus:border-none rounded-md focus:outline-0 focus:ring-blue-500 focus:ring-1 ml-4"
-                                type="text"
-                                placeholder="Add Metrics"
-                                onChange={(e) => setNewObjective(e.target.value)}/>
-                        </div>
+  return (
+    <div>
+      <div className="mx-8 my-4 flex flex-row">
+        <div className="border-black border-2 px-4 py-8 space-y-4 rounded-md">
+          <p className="font-bold text-3xl">Create Objective Form</p>
+          <div className="space-y-4">
+            <div className="space-x-4 max-w-2xl px-4">
+              <label htmlFor="objective-input" className="ml-4">New Objective</label>
+              <input
+                className="w-full px-1 py-2 border border-blue-400 focus:border-none rounded-md focus:outline-0 focus:ring-blue-500 focus:ring-1 "
+                type="text"
+                placeholder="Add Objective"
+                onChange={(e) => setNewObjective(e.target.value)}/>
+            </div>
+
+            {
+              keyResults?.map((keyResult, index) => (
+                <>
+                  <div id="new-key-result" className="space-y-2 bg-gray-50 rounded-md py-2" key={index}>
+                    <div className="space-x-4 max-w-2xl px-4">
+                      <label htmlFor="objective-input" className="ml-4">New KeyResults</label>
+                      <input
+                        className="w-full px-1 py-2 border border-blue-400 focus:border-none rounded-md focus:outline-0 focus:ring-blue-500 focus:ring-1 "
+                        type="text"
+                        placeholder="Add KeyResult Title"
+                        onChange={(e) => {
+                          keyResult.title = e.target.value;
+                          setKeyResults([...keyResults])
+                        }}/>
                     </div>
-                    <div className="grid justify-items-center">
-                    <button
-                        className="bg-blue-400 px-2 py-1 rounded-md text-white ring-2 ring-gray-800 hover:bg-blue-500 justify-self-center"
-                        onClick={addObjective}>Add Objective
+                    <div className="flex ml-4">
+
+                      <div className="space-x-4 max-w-2xl px-4">
+                        <input
+                          className="w-full px-1 py-2 border border-blue-400 focus:border-none rounded-md focus:outline-0 focus:ring-blue-500 focus:ring-1 "
+                          type="number"
+                          placeholder="Initial Value"
+                          onChange={(e) => {
+                            keyResult.initialValue = parseInt(e.target.value);
+                            setKeyResults([...keyResults])
+                          }}/>
+                      </div>
+                      <div className="space-x-4 max-w-2xl px-4">
+                        <input
+                          className="w-full px-1 py-2 border border-blue-400 focus:border-none rounded-md focus:outline-0 focus:ring-blue-500 focus:ring-1 "
+                          type="number"
+                          placeholder="Current Value"
+                          onChange={(e) => {
+                            keyResult.currentValue = parseInt(e.target.value);
+                            setKeyResults([...keyResults])
+                          }}/>
+                      </div>
+                      <div className="space-x-4 max-w-2xl px-4">
+                        <input
+                          className="w-full px-1 py-2 border border-blue-400 focus:border-none rounded-md focus:outline-0 focus:ring-blue-500 focus:ring-1 "
+                          type="number"
+                          placeholder="Target Value"
+                          onChange={(e) => {
+                            keyResult.targetValue = parseInt(e.target.value);
+                            setKeyResults([...keyResults])
+                          }}/>
+                      </div>
+                    </div>
+                    <div className="space-x-4 max-w-2xl px-4">
+                      <input
+                        className="px-1 py-2 border border-blue-400 focus:border-none rounded-md focus:outline-0 focus:ring-blue-500 focus:ring-1 ml-4"
+                        type="text"
+                        placeholder="Add Metrics"
+                        onChange={(e) => {
+                          keyResult.metrics = e.target.value;
+                          setKeyResults([...keyResults])
+                        }}/>
+                    </div>
+                    <button>
+                      Delete Key Result
                     </button>
-                        <button
-                            className="bg-blue-400 px-2 py-1 rounded-md text-white ring-2 ring-gray-800 hover:bg-blue-500 justify-self-center"
-                            onClick={addObjective}>Add Objective
-                        </button>
-                    </div>
-                </div>
+                  </div>
+                </>
+              ))
+            }
+
+          </div>
+
+          <button
+            className="bg-gray-400 px-2 py-1 ml-4 rounded-md text-white ring-2 ring-gray-800 hover:bg-gray-500"
+            onClick={addNewKeyResult}>Add Key Result
+          </button>
+          <div className="grid justify-items-center">
+            <button
+              className="bg-blue-400 px-2 py-1 rounded-md text-white ring-2 ring-gray-800 hover:bg-blue-500 justify-self-end"
+              onClick={addObjective}>Add Objective
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="border-2 mx-8 my-4 px-8 py-4 rounded-md border-black space-y-8">
+        {okrs.length > 0 ? (
+          okrs.map((okr) => {
+            return <div className="space-y-4 bg-blue-200 px-2 py-4 rounded-md">
+              <p>Objective Title : {okr.objective}</p>
+              {
+                okr.keyResults.map((keyResult) => {
+                  return <div className="bg-gray-50">
+                    <p> Key Results: </p>
+                    <p>Title : {keyResult.title}</p>
+                    <p>Initial Value : {keyResult.initialValue}</p>
+                    <p>Current Value: {keyResult.currentValue}</p>
+                    <p>Target Value: {keyResult.targetValue}</p>
+                    <p>Metrics : {keyResult.metrics}</p>
+                  </div>;
+                })
+              }
             </div>
-            <div className="border-2 border-black">
-                {objectives.length > 0 ? (
-                    objectives.map((objective) => {
-                        return <p>{objective}</p>;
-                    })
-                ) : (
-                    <p>No Objectives</p>
-                )}
-            </div>
-        </>
-    );
+          })
+        ) : (
+          <div className="grid justify-items-center"><Frown/><p>No Objectives to show</p></div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
